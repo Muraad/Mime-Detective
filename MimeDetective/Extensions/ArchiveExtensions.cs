70018bb447ef9cad6d9da79e269f9d750244a540
Vince Panuccio <panuccio.vince@gmail.com>
2015-10-06 12:33:44 +1100

    Separate extension methods in to specific namespace
    Extensions should not polute intellisense namespace. Developers should be
    allowed to bring in the extensions they need for the task at hand.
    
    Extensions for different types have been moved in to specific namespaces for
    related tasks.

---
+++ MimeDetective/Extensions/ArchiveExtensions.cs
@@ -0,0 +1,34 @@
+﻿namespace MimeDetective.Extensions.Archives
+{
+    using System.IO;
+
+    /// <summary>
+    /// A set of extension methods for use with document formats.
+    /// </summary>
+    public static class ArchiveExtensions
+    {
+        /// <summary>
+        /// Determines whether the specified file is zip archive
+        /// </summary>
+        /// <param name="fileInfo">The file info.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified file info is zip; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsZip(this FileInfo fileInfo)
+        {
+            return fileInfo.IsType(MimeTypes.ZIP);
+        }
+
+        /// <summary>
+        /// Determines whether the specified file is RAR-archive.
+        /// </summary>
+        /// <param name="fileInfo">The FileInfo.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified file info is RAR; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsRar(this FileInfo fileInfo)
+        {
+            return fileInfo.IsType(MimeTypes.RAR);
+        }
+    }
+}
---
+++ MimeDetective/Extensions/GraphicsExtensions.cs
@@ -0,0 +1,46 @@
+﻿namespace MimeDetective.Extensions.Graphics
+{
+    using System.IO;
+
+    /// <summary>
+    /// A set of extension methods for use with graphics formats.
+    /// </summary>
+    public static class GraphicsExtensions
+    {
+        /// <summary>
+        /// Determines whether the specified file is PNG.
+        /// </summary>
+        /// <param name="fileInfo">The FileInfo object</param>
+        /// <returns>
+        ///   <c>true</c> if the specified file info is PNG; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsPng(this FileInfo fileInfo)
+        {
+            return fileInfo.IsType(MimeTypes.PNG);
+        }
+
+        /// <summary>
+        /// Determines whether the specified file is GIF image
+        /// </summary>
+        /// <param name="fileInfo">The FileInfo object</param>
+        /// <returns>
+        ///   <c>true</c> if the specified file info is GIF; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsGif(this FileInfo fileInfo)
+        {
+            return fileInfo.IsType(MimeTypes.GIF);
+        }
+
+        /// <summary>
+        /// Determines whether the specified file is JPEG image
+        /// </summary>
+        /// <param name="fileInfo">The FileInfo.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified file info is JPEG; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsJpeg(this FileInfo fileInfo)
+        {
+            return fileInfo.IsType(MimeTypes.JPEG);
+        }
+    }
+}
